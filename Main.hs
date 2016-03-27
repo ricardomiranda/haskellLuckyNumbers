06de468a0c330106f6a0d740a328331d856b6920
ricardoMiranda <mail@ricardoMiranda.com>
2016-03-27 21:20:34 +0000

    Some IO

--- Main.hs
+++ Main.hs
@@ -1,13 +1,13 @@
 import System.Environment
+import System.Exit
 
+data Lucky = Lucky | EvenLucky 
 
 oddNumbers :: [Int]
 oddNumbers = filter odd [1..]
 
-
 evenNumbers :: [Int]
 evenNumbers = filter even [1..]
-
 
 luckyNumbers :: [Int] -> [Int]
 luckyNumbers xs = 
@@ -17,14 +17,25 @@ luckyNumbers xs =
       sieve i (ln:s:xs) =
         ln : sieve (i + 1) (s : [x | (n, x) <- zip [i..] xs, rem n s /= 0])
 
+nth :: Int -> Lucky -> Int
+nth j l = case l of 
+  Lucky -> last (take j (luckyNumbers oddNumbers))
+  EvenLucky -> last (take j (luckyNumbers evenNumbers))
 
-lastN :: Int -> Int
-lastN j = 
-  last (take j (luckyNumbers oddNumbers))
-
+lucky :: [String] -> Lucky
+lucky xs =
+ if (not (null (filter (== "evenLucky") xs)))
+   then EvenLucky
+   else Lucky
 
 main :: IO ()
 main = do
-  j <- getArgs
---  putStrLn (show (lastN j))
-  putStrLn (show (3))
+  args <- getArgs
+  if null args
+    then do
+      print "Invalid input, missing arguments"
+      exitWith ExitSuccess
+    else 
+      let l = lucky args in do
+        print (nth (read (head args) :: Int) l)
+        exitWith ExitSuccess
